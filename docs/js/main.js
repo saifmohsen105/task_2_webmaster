35547bb9d8e3ee8a5d6fa93b39dad762d7a5fe34
saifmohsen105 <saifmohsen105@gmail.com>
2025-04-29 22:10:16 +0200

    task_3_webmaster

---
+++ docs/js/main.js
@@ -0,0 +1,64 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        main: '#ff2c1f',
+        textColor: '#020307',
+        bgColor: '#fff'
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif']
+      },
+      backgroundImage: {
+        'home-bg': "url('/img/home3.jpg')"
+      }
+    }
+  }
+}
+
+
+
+// let landingPage = document.querySelector(".home");
+
+// let imageArray = ["home1.jpg" , "home2.jpg" , "home3.jpg"];
+
+// landingPage.style.backgroundImage = 'url("img/home1.jpg)';
+
+
+// let randomNumber = Math.floor(Math.random() * imageArray.length );
+
+
+// let randomOption = true;
+
+
+// function randomizeImgs(){
+//   if( randomOption === true){
+//     setInterval(() => {
+
+//       let randomNumber = Math.floor(Math.random() * imageArray.length );
+    
+//       landingPage.style.backgroundImage = 'url("img/'+  imageArray[randomNumber] +'")';
+    
+//     }, 4000);
+    
+//   }
+// }
+// randomizeImgs();
+
+let header = document.querySelector("header");
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+
+
+
+menuIcon.onclick = () => {
+  navbar.classList.toggle("active");
+}
+window.onscroll = () => {
+  navbar.classList.remove("active");
+}
+window.addEventListener('scroll', () => {
+    header.classList.toggle("bg-black" , window.scrollY > 0);
+});
+
